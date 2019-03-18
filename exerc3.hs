5d690cbc3f5bb5abd2b286658f48838e54906f72
GusAraujo94 <48491916+GusAraujo94@users.noreply.github.com>
2019-03-18 16:22:43 -0300

    Add files via upload

---
+++ exerc3.hs
@@ -0,0 +1,46 @@
+--Q1
+--calcula a soma dos quadrados de dois números x e y
+sumSquares :: Int -> Int -> Int
+sumSquares x y = x^2 + y^2
+
+--Q2
+--verifica se 2 listas possuem o mesmo primeiro elemento
+hasEqHeads :: [Int] -> [Int] -> Bool
+hasEqHeads lis1 lis2 = if head lis1 == head lis2 then True else False
+
+--Q3
+--recebe uma lista de nomes e adiciona a string "Super" no inicio de cada nome
+insSuper :: String -> String
+insSuper lis = "Super " ++ lis
+
+--Q4
+--recebe uma string e retorna o numero de espaços nela contidos
+--contSpace :: String -> Char -> Int
+--contSpace 
+
+--Q5
+--dada uma lista de numeros, calcula 3*n^2 + 2/n +1 para cada numero n da lista
+lisCalc :: [Int] -> [Int]
+lisCalc x = 3*x^2 + 2/n + 1
+
+--Q6
+--dada uma lista de numeros, seleciona somente os que forem negativos
+
+
+
+--Q7
+
+
+
+
+--Q8
+
+
+
+
+--Q9
+
+
+
+
+--Q10
